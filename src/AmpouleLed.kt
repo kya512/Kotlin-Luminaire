588b7ce38904e3853708322d2ee04dcad0570e51
Kelly <158290487+kya512@users.noreply.github.com>
2024-04-23 21:43:19 +0200

    Update AmpouleLed.kt

--- src/AmpouleLed.kt
+++ src/AmpouleLed.kt
@@ -1,4 +1,10 @@
-class AmpouleLed : Ampoule(){
+class AmpouleLed: Eclairage {
+
+    var etat = 0
+
+    fun AmpouleLed() {
+        this.etat = 0
+    }
 
     private fun probaPanne(): Int {
         val alea = (0..100).random() // generated random from 0 to 100 included
@@ -7,4 +13,31 @@ class AmpouleLed : Ampoule(){
         } else 0
     }
 
+    override fun allumer() {
+        if (this.etat != -1 && this.probaPanne() != -1) {
+            this.etat = 10;
+        } else {
+            this.etat = -1;
+        }
+    }
+
+    override fun eteindre() {
+        if (this.etat != -1) {
+            this.etat = 0;
+        }
+    }
+
+    override fun intensifier() {
+            this.etat;
+    }
+
+    override fun diminuer() {
+            this.etat;
+    }
+
+    override fun etat(): Int = this.etat
+
+    override fun toString(): String {
+        return this.javaClass.name + " : " + this.etat
+    }
 }
