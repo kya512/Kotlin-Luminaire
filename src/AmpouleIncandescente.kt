a4921fa17d6b6e8c7f582c181c062252be4e4791
Kelly <158290487+kya512@users.noreply.github.com>
2024-04-23 21:42:55 +0200

    Update AmpouleIncandescente.kt

--- src/AmpouleIncandescente.kt
+++ src/AmpouleIncandescente.kt
@@ -1,8 +1,48 @@
-class AmpouleIncandescente: Ampoule() {
+class AmpouleIncandescente: Eclairage {
+
+    var etat = 0
+
+    fun AmpouleIncandescente() {
+        this.etat = 0
+    }
+
     private fun probaPanne(): Int {
         val alea = (0..100).random() // generated random from 0 to 100 included
         return if (alea < 10) {
             -1
         } else 0
+    }
+
+    override fun allumer() {
+        if (this.etat != -1 && this.probaPanne() != -1) {
+            this.etat = 10;
+        } else {
+            this.etat = -1;
+        }
+    }
+
+    override fun eteindre() {
+        if (this.etat != -1) {
+            this.etat = 0;
+        }
+    }
+
+    override fun intensifier() {
+        if ( this.etat < 10) {
+            this.etat++;
+        }
+    }
+
+    override fun diminuer() {
+        if ( this.etat > 0) {
+            this.etat--;
+        }
+    }
+
+    override fun etat(): Int = this.etat
+
+    override fun toString(): String {
+        return this.javaClass.name + " : " + this.etat
+        }
     }
 }
